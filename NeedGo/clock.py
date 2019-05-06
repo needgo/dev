6f271aa51535ada786bb46653370385578bc9f55
Alejandro Sánchez Villarín <alesanvil3@alum.us.es>
2019-05-06 19:22:11 +0200

    Test cron job.

---
+++ NeedGo/clock.py
@@ -0,0 +1,24 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NeedGo.settings')
+django.setup()
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from NeedGo.app.mapbox_manager import update_mapbox
+
+scheduler = BlockingScheduler()
+
+
+@scheduler.scheduled_job('cron', id='update_mapbox_function_id', minute='*')
+def update_mapbox_function():
+
+    """
+    CRON job to update mapbox.
+    TRIGGER: Every minute.
+    """
+
+    update_mapbox()
+
+
+scheduler.start()
