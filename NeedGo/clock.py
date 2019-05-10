0cbdc772ecbf39233c84a3e2bdb45d120750840c
Alejandro Sánchez Villarín <alesanvil3@alum.us.es>
2019-05-10 11:13:27 +0200

    Test cron job.

--- NeedGo/clock.py
+++ NeedGo/clock.py
@@ -11,7 +11,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job('interval', id='update_mapbox_function_id', seconds=10)
+@scheduler.scheduled_job('interval', id='update_mapbox_function_id', seconds=15)
 def update_mapbox_function():
 
 
